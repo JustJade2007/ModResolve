51bfb20f2e97f399d07f647929d221e9dca35b71
Jacob Hite (EnderBoii) <enderboii266@gmail.com>
2025-08-12 21:28:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
           fontInter.variable,
           fontSourceCodePro.variable
         )}
+        suppressHydrationWarning
       >
         <ThemeProvider
           attribute="class"
